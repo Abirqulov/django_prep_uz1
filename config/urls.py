d2ccc96bef9c7ebf58d84c6d0875cdde804b5118
Abirqulov <80267224+Abirqulov@users.noreply.github.com>
2021-10-11 15:06:01 +0500

    vrefe

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import patterns, url
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
