a6174389c84ecf25f7437e3365d64b29cb7dd5e9
Abirqulov <80267224+Abirqulov@users.noreply.github.com>
2021-10-11 15:34:08 +0500

    dadad

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -28,7 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('routes.api')),
 
-    # re_path(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
